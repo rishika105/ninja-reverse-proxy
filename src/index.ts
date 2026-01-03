b58aa2612b58ef6c311d57a6ec9bb36bc60bec40
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2026-01-03 23:16:05 +0530

    added userconfig and main proxy code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-console.log("Hello")
+import { program } from "commander";
+import { parseYAMLConfig, validateConfig } from "./config.js";
 
+interface CreateServerConfig {
+    port: number;
+    workerCount: number;
+}
+
+async function createServer(config:CreateServerConfig) {
+    const {workerCount} = config;
+
+    const workers = new Array(workerCount);
+    
+}
+
+async function main() {
+    program.option('--config <path>');
+    program.parse();
+
+    const options = program.opts();
+
+    if(options && 'config' in options){
+        const validatedConfig = await validateConfig(await parseYAMLConfig(options.config))
+    }
+
+
+}
+
+main();
