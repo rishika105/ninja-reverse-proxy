3211f421d9e19db7046d293e1ab0b3575c88454c
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2026-01-12 23:08:01 +0530

    update

--- src/schema/server-schema.ts
+++ src/schema/server-schema.ts
@@ -1,10 +1,17 @@
-import {z} from "zod";
+import { z } from "zod";
 
-const workerMessageSchema = z.object({
+export const workerMessageSchema = z.object({
     requestType: z.enum(["HTTP"]),
     headers: z.any(),
     body: z.any(),
     url: z.string(),
 });
 
+export const workerMessageReplySchema = z.object({
+    data: z.string(),
+    error: z.string().optional(),
+    errorCode: z.enum(['500', '404'])
+})
+
 export type WorkerMessageType = z.infer<typeof workerMessageSchema>
+export type WorkerMessageReplyType = z.infer<typeof workerMessageSchema>
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import cluster, { type Worker } from "node:cluster";
 import http from "node:http"
 import { rootConfigSchema, type ConfigSchemaType } from "./schema/config-schema.js";
-import type { WorkerMessageType } from "./schema/server-schema.js";
+import type { WorkerMessageType, WorkerMessageReplyType } from "./schema/server-schema.js";
+import { workerMessageSchema } from "./schema/server-schema.js"
 
 interface CreateServerConfig {
     port: number;
@@ -13,22 +14,28 @@ interface CreateServerConfig {
 export async function createServer(config: CreateServerConfig) {
     const { workerCount, port } = config;
 
+    const WORKER_POOL: Worker[] = [];
+
+
     //master node -> primary
     if (cluster.isPrimary) {
         console.log("Master process is up 🚀 ")
 
         for (let i = 0; i < workerCount; i++) {
             //this fork takes an env here config
-            cluster.fork({ config: JSON.stringify(config.config) }); //spawn a new worker process
+            const w = cluster.fork({ config: JSON.stringify(config.config) }); //spawn a new worker process
+            WORKER_POOL.push(w)
             console.log(`Master process: worker node spinned ${i}`)
         }
 
 
         const server = http.createServer(function (req, res) {
             //random index of a worker
-            const index = Math.floor(Math.random() * workerCount)
+            const index = Math.floor(Math.random() * WORKER_POOL.length)
             //workers is not nullable we know so add !
-            const worker: Worker = Object.values(!cluster.workers)[index];
+            const worker = WORKER_POOL.at(index);
+
+            if (!worker) throw new Error('Worker not found')
 
             const payload: WorkerMessageType = {
                 requestType: 'HTTP',
@@ -40,8 +47,8 @@ export async function createServer(config: CreateServerConfig) {
             worker.send(JSON.stringify(payload))
         })
 
-        server.listen(config.port, function() {
-            console.log(`Reverse proxy listening on PORT ${port}` )
+        server.listen(config.port, function () {
+            console.log(`Reverse proxy listening on PORT ${port}`)
         })
     }
     else {
@@ -49,8 +56,44 @@ export async function createServer(config: CreateServerConfig) {
         // console.log(process.env.config)
         const config = await rootConfigSchema.parseAsync(JSON.parse(`${process.env.config}`));
 
-        process.on('message', (message) => {
-            console.log(message)
+        process.on('message', async (message: string) => {
+            const messageValidated = await workerMessageSchema.parseAsync(JSON.parse(message))
+
+            const requestURL = messageValidated.url
+            const rule = config.server.rules.filter(e => e.path === requestURL)
+
+            if (!rule) {
+                const reply: WorkerMessageReplyType = {
+                    errorCode: '500',
+                    error: 'Rule not found'
+                }
+
+                if (process.send) return process.send(JSON.stringify(reply));
+            }
+
+
+            const upstreamID = rule?.upstreams[0];
+            const upstream = config.server.upstreams.find(e => e.id === upstreamID)
+
+            if (!upstream) {
+                const reply: WorkerMessageReplyType = {
+                    errorCode: '500',
+                    error: 'Upstream not found'
+                }
+
+                if (process.send) return process.send(JSON.stringify(reply));
+            }
+
+            http.request({host: upstream?.url, path: requestURL})
+
+
+
+
+
+
+
+
+            console.log('WORKER', message)
         })
     }
 }
